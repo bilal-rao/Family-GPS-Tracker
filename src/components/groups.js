346bc4d76b7478b171046807be280c9a9686d938
Muneeb JS <muneeb@magnitt.com>
2019-10-23 14:05:39 +0400

    Removed unused stuffs

--- src/components/groups.js
+++ src/components/groups.js
@@ -1,29 +1,17 @@
 import React, { Component } from 'react';
-import CreateGroup from './createGroup.js';
-import Map from './Map.js';
 import { connect } from 'react-redux';
-import * as firebase from 'firebase';
-import Members from './members.js';
 import { Actions } from 'react-native-router-flux';
 import store from './../store/index.js';
 import MiddlewareCircle from './../store/middleWares/middlewareCircle.js';
-import { Container, Header, Content, Card, Spinner, CardItem, Footer, FooterTab, Thumbnail, Title, Right, Icon, Body, Left, List, ListItem } from 'native-base';
+import { Spinner } from 'native-base';
 import {
-    AsyncStorage,
-    AppRegistry,
     StyleSheet,
     Text,
     View,
     ScrollView,
-    TouchableOpacity,
-    TextInput,
-    Button,
-    Image,
-    Dimensions, Picker,
-    BackAndroid,
-    ToastAndroid,
-    Alert
+    TouchableOpacity
 } from 'react-native';
+
 function mapStateToProps(state) {
     return {
         circleData: state.reducerCircle,
@@ -38,40 +26,25 @@ class Groups extends Component {
     }
     componentWillMount() {
         store.dispatch(MiddlewareCircle.asyncCircle());
-        console.log("WillMount", this.props);
-        // var Rootref = firebase.database().ref().child("groups").orderByChild('admin').equalTo(firebase.auth().currentUser.uid);
-        // if(Rootref!=null){
-        // Rootref.on("value", snap => {
-        //     var data = snap.val();
-        //     console.log("data",data);
-        //     let gData = [];
-        //     for (let key in data) {
-        //         data[key].key=key
-        //         gData.push(data[key])
-        //         // keys.push(key)
-        //     }
-        //     this.setState({
-        //         data: gData,
-        //     })
-        // })
-        // }
     }
+
     componentWillReceiveProps(props) {
         console.log(props);
         this.setState({
             data: props.circleData,
         })
     }
+
     createGroup = () => {
         Actions.CreateGroup();
     }
+
     list = (data) => {
         console.log(data)
         Actions.Members({ data: data });
     }
+
     render() {
-        // console.log(this.state.data)
-        // console.log(this.state.key)
         return (
             <View style={styles.container}>
                 {this.state.data[0] ?
@@ -98,29 +71,6 @@ class Groups extends Component {
     }
 }
 
-// <Container>
-//     <Content>
-//         {this.state.data ?
-//             <List avatar
-//                 dataArray={this.state.data}
-//                 renderRow={(d) =>
-//                     <ListItem button onPress={() => this.list(d)}>
-//                         <Body>
-//                             <Text>{d.gName}</Text>
-//                         </Body>
-//                     </ListItem>
-//                 }>
-//             </List>
-//             :
-//             <View> <Text>Empty</Text></View>
-//         }
-//         <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-//             <TouchableOpacity onPress={() => this.createGroup()} >
-//                 <Text>Create Group</Text>
-//             </TouchableOpacity>
-//         </View>
-//     </Content>
-// </Container>
 export default connect(mapStateToProps)(Groups)
 const styles = StyleSheet.create({
     container: {
@@ -138,11 +88,7 @@ const styles = StyleSheet.create({
     },
     header: {
         backgroundColor: '#38b759',
-        height: 60,
-        // alignItems: 'center',
-        // justifyContent: 'center',
-        // borderBottomWidth: 10,
-        // borderBottomColor: '#ddd'
+        height: 60
     },
     headerText: {
         color: 'white',
